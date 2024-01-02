90991fe4b57261ee005cd0fb2cba5d912f5987c7
Matthias Anton <facelessleader@gmail.com>
2024-01-02 21:09:19 +0100

    added deepcopy to activeRota in core

--- Core.lua
+++ Core.lua
@@ -49,9 +49,29 @@ end
 
 local acitveRota
 
+local function deepcopy(o, seen)
+    seen = seen or {}
+    if o == nil then return nil end
+    if seen[o] then return seen[o] end
+  
+    local no
+    if type(o) == 'table' then
+      no = {}
+      seen[o] = no
+  
+      for k, v in next, o, nil do
+        no[deepcopy(k, seen)] = deepcopy(v, seen)
+      end
+      setmetatable(no, deepcopy(getmetatable(o), seen))
+    else -- number, string, boolean, etc
+      no = o
+    end
+    return no
+  end
+
 function DPSGenie:SetActiveRota(rotaTable)
     DPSGenie:Print("Setting active Rota to: " .. rotaTable.name)
-    acitveRota = rotaTable
+    acitveRota = deepcopy(rotaTable)
     DPSGenie:SaveSettingToProfile("activeRota", rotaTable)
     -- build cache with, spellinfo, harmful, helpful
 end
